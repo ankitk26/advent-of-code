a0b5c74422931a06d2c60da34ecb390439115d3a
ankitk26 <myselfankit51@gmail.com>
2025-12-02 04:57:39 +0530

    2025 day 1

---
+++ 2025/day_1_a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  int n = 4145;
+
+  int dial = 50;
+  int ans = 0;
+
+  while (n--) {
+    string s; cin >> s;
+    char dir = s[0];
+
+    int k = stoi(s.substr(1, s.size() - 1));
+    k %= 100;
+
+    if (dir == 'L') {
+      dial -= k;
+      if (dial < 0) {
+        dial += 100;
+      }
+    } else {
+      dial += k;
+      dial %= 100;
+    }
+    if (dial == 0) {
+      ans++;
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
