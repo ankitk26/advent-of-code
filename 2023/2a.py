ce401ae3ff2f65c2fafae5c32c5a574c51bec433
ankitk26 <myselfankit51@gmail.com>
2025-12-05 06:31:55 +0530

    2023 2a in python

---
+++ 2023/2a.py
@@ -0,0 +1,38 @@
+n = 100
+
+tests: list[str] = []
+while n != 0:
+    s = input()
+    tests.append(s)
+    n -= 1
+
+RED, BLUE, GREEN = 12, 14, 13
+
+sum = 0
+for index, test in enumerate(tests):
+    flag = 0
+
+    sets = [x.strip() for x in test.split(":")][1]
+    draws = [x.strip() for x in sets.split(";")]
+    for draw in draws:
+        if flag == 1:
+            break
+        cubes = [cube.strip() for cube in draw.split(",")]
+        for cube in cubes:
+            if flag == 1:
+                break
+            [count, color] = [x.strip() for x in cube.split(" ")]
+
+            actual_count = int(count)
+
+            if color == "blue" and actual_count > BLUE:
+                flag = 1
+            if color == "red" and actual_count > RED:
+                flag = 1
+            if color == "green" and actual_count > GREEN:
+                flag = 1
+
+    if flag == 0:
+        sum += index + 1
+
+print(sum)
